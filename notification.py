0a2dd03ba281307eb7bfe410f00bbb3292b9b8f7
KucielKrzysztof <s223871@student.uek.krakow.pl>
2024-02-06 16:33:01 +0100

    lets the user add notifications from the prompt

--- notification.py
+++ notification.py
@@ -6,6 +6,7 @@ from pystray import Icon, MenuItem as item
 from PIL import Image
 from threading import Thread, Event
 import pygame
+#import tkinter as tk
 
 class CustomNotification:
     def __init__(self, title, message, hour, minute):
@@ -20,18 +21,18 @@ def show_notification(title, message):
 class App:
     def __init__(self):
         self.exit_event = Event()
-        self.notifications = [
-            #todo: let the user add externally
-            CustomNotification("GO TO WORK!!!!", "test1", 0, 18),
-            CustomNotification("GO TO WORK!!!!", "test2", 23, 58,)
-            # YES IKR ADDING IT MANUALLY IS XD
-        ]
-
+        self.notifications = []
         menu = (item('SUPER stupid Notifications', self.click), item('Close', self.exit_program))
-
         self.icon = Icon("name", Image.open("assets/icon.png"), "SUPER stupid Notifications", menu=menu)
-
         pygame.init()
+
+    def add_notification(self, title, message, hour, minute):
+        #title = input("Title: ")
+        #message = input("Message: ")
+        #hour = int(input("Hour (0-23): "))
+        #minute = int(input("Minute (0-59): "))
+        new_notification = CustomNotification(title, message, hour, minute)
+        self.notifications.append(new_notification)
 
     def show_notification(self, title, message):
         ctypes.windll.user32.MessageBoxW(0, message, title, 1)
@@ -55,12 +56,16 @@ class App:
                 if current_time.hour == notification.hour and current_time.minute == notification.minute:
                     self.play_sound("assets/alert.mp3")
                     self.show_notification(notification.title, notification.message)
-            time.sleep(60)
+            time.sleep(10)
 
     def main(self):
+        while True:
+            self.add_notification(input("Title: "), input("Message: "), int(input("Hour (0-23): ")), int(input("Minute (0-59): ")))
+            add_more = input("Do you want to add more notifications? (y/n): ")
+            if add_more.lower() != 'y':
+                break
         notifications_thread = Thread(target=self.check_notifications)
         notifications_thread.start()
-
         self.icon.run()
 
 if __name__ == "__main__":
